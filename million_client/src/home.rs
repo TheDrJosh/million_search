28146e3d29d2f4008ab86c9077ba01885a01af83
TheDrJosh <51982649+TheDrJosh@users.noreply.github.com>
2024-05-05 17:44:23 -0700

    search page

--- million_client/src/home.rs
+++ million_client/src/home.rs
@@ -4,6 +4,20 @@ use maud::{html, Markup};
 use crate::{utils::basic_page, SearchType};
 
 pub async fn home_search_page(search_type: SearchType) -> Result<Markup, StatusCode> {
+    let sub_text = match search_type {
+        SearchType::Html => None,
+        SearchType::Image => Some("Images"),
+        SearchType::Video => Some("Videos"),
+        SearchType::Audio => Some("Audio"),
+    };
+
+    let search_url = match search_type {
+        SearchType::Html => "/search",
+        SearchType::Image => "/image/search",
+        SearchType::Video => "/video/search",
+        SearchType::Audio => "/audio/search",
+    };
+
     Ok(basic_page(html! {
         div class="h-lvh flex flex-col items-center justify-center dark:bg-zinc-800 dark:text-zinc-50" {
             div class="flex-1" {}
@@ -12,25 +26,12 @@ pub async fn home_search_page(search_type: SearchType) -> Result<Markup, StatusC
                 h1 class="font-bold tracking-tight text-6xl mb-2 text-center" {
                     "Million Search"
                 }
-                @match search_type {
-                    SearchType::Html => {}
-                    SearchType::Image => {
-                        span class="trackinng-tight text-2xl mb-10 text-center" {
-                            "Images"
-                        }
-                    }
-                    SearchType::Video => {
-                        span class="trackinng-tight text-2xl mb-10 text-center" {
-                            "Videos"
-                        }
-                    }
-                    SearchType::Audio => {
-                        span class="trackinng-tight text-2xl mb-10 text-center" {
-                            "Audio"
-                        }
+                span class="trackinng-tight text-2xl mb-10 text-center" {
+                    @if let Some(sub_text) = sub_text {
+                        (sub_text)
                     }
                 }
-                form action="/search" autocomplete="off" class="flex flex-row items-center" {
+                form action=(search_url) autocomplete="off" class="flex flex-row items-center" {
                     object data="public/search.svg" type="image/svg+xml" class="h-4 -mr-8 z-10 filter dark:invert" {}
 
                     input class="border-black border resize-none pl-10 pr-4 py-2 rounded-full hover:bg-neutral-100 dark:bg-zinc-800 dark:border-zinc-700 dark:hover:bg-zinc-700"
--- million_client/src/search.rs
+++ million_client/src/search.rs
@@ -1,14 +1,79 @@
 use std::sync::Arc;
 
 use axum::http::StatusCode;
-use maud::Markup;
+use maud::{html, Markup};
 
-use crate::{AppState, SearchQuery, SearchType};
+use crate::{utils::basic_page, AppState, SearchQuery, SearchType};
 
 pub async fn search_page(
     search_type: SearchType,
     query: SearchQuery,
     state: Arc<AppState>,
 ) -> Result<Markup, StatusCode> {
-    todo!()
+    Ok(basic_page(html! {
+        div class="min-h-lvh flex flex-col items-start dark:bg-zinc-800 dark:text-zinc-50 overflow-hidden" {
+
+            header class="flex flex-row py-6 border-b-2 border-neutral-200 dark:border-zinc-700 w-full" {
+                @match search_type {
+                    SearchType::Html => {
+                        a href="/" {
+                            h1 class="font-bold tracking-tight text-3xl ml-6 mr-12 text-center" {
+                                "Million Search"
+                            }
+                        }
+                    }
+                    SearchType::Image => {
+                        a href="/image" {
+                            h1 class="font-bold tracking-tight text-3xl ml-6 mr-12 text-center" {
+                                "Million Search"
+                            }
+                            span {
+                                "Images"
+                            }
+                        }
+                    }
+                    SearchType::Video => {
+                        a href="/video" {
+                            h1 class="font-bold tracking-tight text-3xl ml-6 mr-12 text-center" {
+                                "Million Search"
+                            }
+                            span {
+                                "Videos"
+                            }
+                        }
+                    }
+                    SearchType::Audio => {
+                        a href="/audio" {
+                            h1 class="font-bold tracking-tight text-3xl ml-6 mr-12 text-center" {
+                                "Million Search"
+                            }
+                            span {
+                                "Audio"
+                            }
+                        }
+                    }
+                }
+
+                form action="/search" autocomplete="off" class="flex flex-row items-center" {
+                    object data="/public/search.svg" type="image/svg+xml" class="h-4 -mr-8 z-10 filter dark:invert" {}
+
+                    input class="border-black border resize-none pl-10 pr-4 py-2 rounded-full hover:bg-neutral-100 dark:bg-zinc-800 dark:border-zinc-700 dark:hover:bg-zinc-700"
+                        type="search" name="query" id="query" size="60" value=(query.query) {}
+                }
+            }
+
+            div class="flex-1 mx-6 my-4 overflow-y-scroll w-full" {
+                // @for result in &response {
+                //     (render_result(result))
+                // }
+                // @if response.len() == INITIAL_QUERY_LENGTH {
+                //     div hx-post="/search" hx-trigger="intersect once" hx-swap="outerHTML" hx-vals={"{\"query\": " (query.query) "\", start\": " (INITIAL_QUERY_LENGTH) "}"} {}
+                // }
+            }
+
+            footer class="bg-neutral-100 grid dark:bg-zinc-900 grid-cols-3 w-full px-4 py-2 gap-2" {
+                a href="https://dryicons.com/icon/search-2621" {"Icon by Dryicons"}
+            }
+        }
+    }))
 }
